70b700a56ae36657c6725003ef981bd05d2a4814
Tubsterlol <asitpatel2005@gmail.com>
2026-09-07 11:59:18 +0530

    logo

--- lua/config/alpha.lua
+++ lua/config/alpha.lua
@@ -2,12 +2,24 @@ local alpha = require("alpha")
 local dashboard = require("alpha.themes.dashboard")
 
 dashboard.section.header.val = {
-  " ███╗   ██╗██╗   ██╗██╗███╗   ███╗",
-  " ████╗  ██║██║   ██║██║████╗ ████║",
-  " ██╔██╗ ██║██║   ██║██║██╔████╔██║",
-  " ██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║",
-  " ██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║",
-  " ╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝",
+  "                            .oodMMMM",
+  "                   .oodMMMMMMMMMMMMM",
+  "       ..oodMMM  MMMMMMMMMMMMMMMMMMM",
+  " oodMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM",
+  " MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM",
+  " MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM",
+  " MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM",
+  " MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM",
+  " MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM",
+  "",
+  " MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM",
+  " MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM",
+  " MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM",
+  " MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM",
+  " MMMMMMMMMMMMMM  MMMMMMMMMMMMMMMMMMM",
+  " `^^^^^^MMMMMMM  MMMMMMMMMMMMMMMMMMM",
+  "       ````^^^^  ^^MMMMMMMMMMMMMMMMM",
+  "                      ````^^^^^^MMMM",
 }
 
 dashboard.section.buttons.val = {
